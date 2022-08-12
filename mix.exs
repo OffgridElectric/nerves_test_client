2230cbd3934d4a955578b5a34e4d5c0b45a63900
Jean Parpaillon <jean.parpaillon@free.fr>
2022-08-12 16:34:50 +0200

    Replace vintage_net dep with more complete nerves_pack dep

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule NervesTestClient.MixProject do
       {:jason, "~> 1.0"},
       {:nerves_hub_link, "~> 1.2"},
       {:slipstream, "~> 1.0"},
-      {:nerves_runtime, "~> 0.7"}
+      {:nerves_pack, "~> 0.7"}
     ]
   end
 end
